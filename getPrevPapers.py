5b80377b6759b76aaa74df09596f2bc950667c41
Manohar Reddy <f20180564@hyderabad.bits-pilani.ac.in>
2020-04-13 23:59:46 +0530

    Use remote server if not in BPHC campus

--- getPrevPapers.py
+++ getPrevPapers.py
@@ -5,12 +5,17 @@ from bs4 import BeautifulSoup
 courseCode = input("Enter course code with spaces: ")
 courseCode = courseCode.upper()
 os.mkdir(courseCode)
+response = os.system("ping 172.16.100.176 -c 1 -W 1 >/dev/null")
+if(response == 0):
+	baseUrl = "http://172.16.100.176:8080"
+else:
+	baseUrl = "http://14.98.244.199:8080"
 codes = courseCode.split()
-html_doc = urllib.request.urlopen("http://172.16.100.176:8080/jspui/browse?type=author&value="+codes[0]+"+"+codes[1])
+html_doc = urllib.request.urlopen(f"{baseUrl}/jspui/browse?type=author&value="+codes[0]+"+"+codes[1])
 soup = BeautifulSoup(html_doc, 'html.parser')
 for link in soup.find_all('a'):
     if("handle" in link.get('href')):
-        semLink = "http://172.16.100.176:8080"+link.get('href')
+        semLink = baseUrl+link.get('href')
         semHtml_doc = urllib.request.urlopen(semLink)
         semSoup = BeautifulSoup(semHtml_doc, 'html.parser')
         for name in semSoup.find_all("td", class_ = "metadataFieldValue dc_date_issued"):
@@ -19,5 +24,5 @@ for link in soup.find_all('a'):
                 if("bitstream" in inlink.get('href')):
                         linkParts = inlink.get('href').split("/")
                         print(f'downloading {linkParts[-1]}')
-                        urllib.request.urlretrieve("http://172.16.100.176:8080"+inlink.get('href'), "./"+courseCode+"/"+name.get_text()+"/"+urllib.parse.unquote(linkParts[-1]))
+                        urllib.request.urlretrieve(baseUrl+inlink.get('href'), "./"+courseCode+"/"+name.get_text()+"/"+urllib.parse.unquote(linkParts[-1]))
         
